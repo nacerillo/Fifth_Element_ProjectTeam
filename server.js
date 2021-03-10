530d28af5d2086876585b70251e0a9465a2e2093
nacerillo <nicholascerillo@gmail.com>
2021-03-10 14:05:02 -0800

    adding View Details button to city favorites

--- server.js
+++ server.js
@@ -1,36 +1,54 @@
-'use strict';
-require('dotenv').config();
+"use strict";
+require("dotenv").config();
 
-const express = require('express');
-const superagent = require('superagent');
-const pg = require('pg');
-const methodOverride = require('method-override');
+const express = require("express");
+const superagent = require("superagent");
+const pg = require("pg");
+const methodOverride = require("method-override");
 const client = new pg.Client(process.env.DATABASE_URL);
 const app = express();
 const PORT = process.env.PORT || 3003;
 const JOB_API = process.env.JOB_API;
 const JOB_ID = process.env.JOB_ID;
 
-client.on('error', (error) => console.log(error));
+client.on("error", (error) => console.log(error));
 
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', renderHomepage);
-app.post('/search/result', getCity);
-app.post('/search/job', getJob);
-app.get('/favorites', renderFavPage);
-app.post('/favorites', addToFavorites);
-
+app.get("/", renderHomepage);
+app.post("/search/result", getCity);
+app.post("/search/job", getJob);
+app.get("/favorites", renderFavPage);
+app.post("/favorites", addToFavorites);
+app.get("/cities/:id", getSingleCity);
 
 function renderHomepage(req, res) {
-  res.render('pages/index.ejs');
+  res.render("pages/index.ejs");
 }
 
+function getSingleCity(req, res) {
+  const sqlString = "SELECT * FROM city_data WHERE id = $1";
+  const sqlID = [req.params.id];
+
+  client
+    .query(sqlString, sqlID)
+    .then((results) => {
+      console.log("Start Of Results", results, "FAV-RESULT");
+      const city = results.rows[0];
+      res.render("pages/details.ejs", { newCity: city });
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .send("Looks like there's a problem with getting the city.");
+    });
+}
 function getCity(req, res) {
-  const cityname = req.body.city_name.replace(/\s+/g, '-').toLowerCase();
+  const cityname = req.body.city_name.replace(/\s+/g, "-").toLowerCase();
   const checkData = `SELECT * FROM city_data WHERE name = $1`;
   const checkArray = [cityname];
   // console.log(cityname, 'city-name');
@@ -44,27 +62,65 @@ function getCity(req, res) {
         .then((responseArray) => {
           const cityResults = responseArray[0];
           const imageResult = responseArray[1];
-          newCity.description = cityResults.body.summary.replace(/<[^>]*>/g,'');
-          newCity.housing = cityResults.body.categories[0].score_out_of_10.toFixed(2);
-          newCity.cost_of_living = cityResults.body.categories[1].score_out_of_10.toFixed(2);
-          newCity.startup = cityResults.body.categories[2].score_out_of_10.toFixed(2);
-          newCity.venture_capital = cityResults.body.categories[3].score_out_of_10.toFixed(2);
-          newCity.travel_connect = cityResults.body.categories[4].score_out_of_10.toFixed(2);
-          newCity.commute = cityResults.body.categories[5].score_out_of_10.toFixed(2);
-          newCity.buisness = cityResults.body.categories[6].score_out_of_10.toFixed(2);
-          newCity.safety = cityResults.body.categories[7].score_out_of_10.toFixed(2);
-          newCity.healthcare = cityResults.body.categories[8].score_out_of_10.toFixed(2);
-          newCity.education = cityResults.body.categories[9].score_out_of_10.toFixed(2);
-          newCity.enviroment = cityResults.body.categories[10].score_out_of_10.toFixed(2);
-          newCity.economy = cityResults.body.categories[11].score_out_of_10.toFixed(2);
-          newCity.taxation = cityResults.body.categories[12].score_out_of_10.toFixed(2);
-          newCity.internet_access = cityResults.body.categories[13].score_out_of_10.toFixed(2);
-          newCity.culture = cityResults.body.categories[14].score_out_of_10.toFixed(2);
-          newCity.tolerance = cityResults.body.categories[15].score_out_of_10.toFixed(2);
-          newCity.outdoors = cityResults.body.categories[16].score_out_of_10.toFixed(2);
-          newCity.generalScore = cityResults.body.teleport_city_score.toFixed(2);
+          newCity.description = cityResults.body.summary.replace(
+            /<[^>]*>/g,
+            ""
+          );
+          newCity.housing = cityResults.body.categories[0].score_out_of_10.toFixed(
+            2
+          );
+          newCity.cost_of_living = cityResults.body.categories[1].score_out_of_10.toFixed(
+            2
+          );
+          newCity.startup = cityResults.body.categories[2].score_out_of_10.toFixed(
+            2
+          );
+          newCity.venture_capital = cityResults.body.categories[3].score_out_of_10.toFixed(
+            2
+          );
+          newCity.travel_connect = cityResults.body.categories[4].score_out_of_10.toFixed(
+            2
+          );
+          newCity.commute = cityResults.body.categories[5].score_out_of_10.toFixed(
+            2
+          );
+          newCity.buisness = cityResults.body.categories[6].score_out_of_10.toFixed(
+            2
+          );
+          newCity.safety = cityResults.body.categories[7].score_out_of_10.toFixed(
+            2
+          );
+          newCity.healthcare = cityResults.body.categories[8].score_out_of_10.toFixed(
+            2
+          );
+          newCity.education = cityResults.body.categories[9].score_out_of_10.toFixed(
+            2
+          );
+          newCity.enviroment = cityResults.body.categories[10].score_out_of_10.toFixed(
+            2
+          );
+          newCity.economy = cityResults.body.categories[11].score_out_of_10.toFixed(
+            2
+          );
+          newCity.taxation = cityResults.body.categories[12].score_out_of_10.toFixed(
+            2
+          );
+          newCity.internet_access = cityResults.body.categories[13].score_out_of_10.toFixed(
+            2
+          );
+          newCity.culture = cityResults.body.categories[14].score_out_of_10.toFixed(
+            2
+          );
+          newCity.tolerance = cityResults.body.categories[15].score_out_of_10.toFixed(
+            2
+          );
+          newCity.outdoors = cityResults.body.categories[16].score_out_of_10.toFixed(
+            2
+          );
+          newCity.generalScore = cityResults.body.teleport_city_score.toFixed(
+            2
+          );
           newCity.image_url = imageResult.body.photos[0].image.mobile;
-
 
           const addData = `INSERT INTO city_data (name,description,housing,cost_of_living,startup,venture_capital,travel_connect, 
             commute, buisness,safety,healthcare,education,enviroment,economy,taxation,internet_access,culture,tolerance,outdoors,generalScore,image_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`;
@@ -95,29 +151,30 @@ function getCity(req, res) {
             console.log(error);
             res
               .status(500)
-              .send('Looks like there\'s a problem with adding data.');
+              .send("Looks like there's a problem with adding data.");
           });
-          res.render('pages/details.ejs', { newCity: newCity });
+          res.render("pages/details.ejs", { newCity: newCity });
         })
         .catch((error) => {
           console.log(error);
           res.status(500).send(`Sorry something went wrong`);
         });
     } else {
-      console.log('Already Exists');
-      res.render('pages/details.ejs', { newCity: returnedData.rows[0] });
+      console.log("Already Exists");
+      res.render("pages/details.ejs", { newCity: returnedData.rows[0] });
     }
   });
 }
 
-function getJob(req, res){
+function getJob(req, res) {
   const cityname = req.body.city_name;
-  let userInput = req.body.job_query
+  let userInput = req.body.job_query;
   const url = `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${JOB_ID}&app_key=${JOB_API}&results_per_page=5&what_or=${userInput}&where=${cityname}&salary_min=40000&content-type=application/json`;
-  superagent.get(url)
-    .then(result => {
-      const output = result.body.results.map(job => new Job(job))
-      res.render('pages/job', {output})
+  superagent
+    .get(url)
+    .then((result) => {
+      const output = result.body.results.map((job) => new Job(job));
+      res.render("pages/job", { output });
     })
     .catch((error) => {
       console.log(error);
@@ -125,12 +182,13 @@ function getJob(req, res){
     });
 }
 
-function renderFavPage(req, res){
-  const sqlString = 'SELECT * FROM city_fav_data;';
-  client.query(sqlString)
-    .then(result => {
-      const ejsObject = {allCities: result.rows};
-      res.render('pages/favorites.ejs', ejsObject);
+function renderFavPage(req, res) {
+  const sqlString = "SELECT * FROM city_fav_data;";
+  client
+    .query(sqlString)
+    .then((result) => {
+      const ejsObject = { allCities: result.rows };
+      res.render("pages/favorites.ejs", ejsObject);
     })
     .catch((error) => {
       console.log(error);
@@ -138,7 +196,7 @@ function renderFavPage(req, res){
     });
 }
 
-function addToFavorites(req, res){
+function addToFavorites(req, res) {
   const sqlString = `INSERT INTO city_fav_data (name,description,housing,cost_of_living,startup,venture_capital,travel_connect, 
     commute, buisness, safety, healthcare, education, enviroment, economy, taxation, internet_access, culture, tolerance, outdoors,generalScore, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21) RETURNING id;`;
   const sqlArray = [
@@ -162,11 +220,13 @@ function addToFavorites(req, res){
     req.body.tolerance,
     req.body.outdoors,
     req.body.generalScore,
-    req.body.image_url];
-    // console.log(sqlArray)
-  client.query(sqlString, sqlArray)
+    req.body.image_url,
+  ];
+  // console.log(sqlArray)
+  client
+    .query(sqlString, sqlArray)
     .then(() => {
-      console.log('added to DB');
+      // console.log("added to DB");
       res.redirect(`/favorites`);
     })
     .catch((error) => {
@@ -175,8 +235,8 @@ function addToFavorites(req, res){
     });
 }
 
-function Job(object){
-  this.title = object.title.replace(/<[^>]*>/g,'');
+function Job(object) {
+  this.title = object.title.replace(/<[^>]*>/g, "");
   this.company = object.company.display_name;
   this.location = object.location.display_name;
   this.job_link = object.redirect_url;
